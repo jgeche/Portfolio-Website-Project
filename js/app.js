65a2acdf4bd553e38f502b32cd94d0754629de92
Jamie Geche <jamiergeche@gmail.com>
2021-06-30 19:09:42 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -43,3 +43,17 @@ $(document).ready(function () {
 		$(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
 	}
 });
+
+//Fade in Section on Mouse Scroll
+$(window).scroll(function() {
+    $('.fade-in').each(function() {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
+            $(this).addClass('is-visible');
+        }
+    });
+});
